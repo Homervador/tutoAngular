4b5ccecd23fece2e782a16b654b1eda491fe8ccf
Homervador <pierre.puss@gmail.com>
2017-09-08 13:59:46 +0200

    fin de l'exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component ({
+	selector: 'app-root',
+	template: `
+		<h1>{{title}}</h1>
+		<nav>
+			<a routerLink="/heroes">Heroes</a>
+			<a routerLink="/dashboard">Dashboard</a>
+		</nav>
+		<router-outlet></router-outlet>
+		`,
+	styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+	title = 'Tour of Heroes';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from'@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModules } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService} from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModules,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
-  providers: [],
+  providers: [
+  	HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
